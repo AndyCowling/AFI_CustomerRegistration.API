fb7fc493c3acfbd3d78be279e180940be7334ba9
Andy Cowling <andy.cowling@outlook.com>
2020-03-22 21:32:52 +0000

    Added Dbset here and made it a DbContext

--- Models/CustomerDetailContext.cs
+++ Models/CustomerDetailContext.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace AFI_CustomerRegistration.API.Models
 {
-    public class CustomerDetailContext
+    public class CustomerDetailContext : DbContext
     {
+
+
+        public DbSet<CustomerDetail> CustomerDetails { get; set; }
     }
 }
